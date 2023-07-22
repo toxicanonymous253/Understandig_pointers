c67974a76b6c1f17ef82665358615457227bc38b
toxicfrank <ngugifrancis930@gmai.com>
2023-07-22 11:20:20 +0300

    pointer

--- understanding_pointers/pointer_arrays/8-ptr_arr.c
+++ understanding_pointers/pointer_arrays/8-ptr_arr.c
@@ -3,14 +3,15 @@
 int main(void)
 {
 	int a[][4] = {
-		{5, 7, 5, 9},
-		{4, 6, 3, 1},
-		{2, 9, 0, 6},
+		{5, 7, 5, 9},/*An array of 4 intergers*/
+		{4, 6, 3, 1},/*An array of 4 integers*/
+		{2, 9, 0, 6},/*An array of 4 integers*/
 	};
 	int *p;
-	int (*q)[4];
-	p = (int *)a;
-	q = a;
+	int (*q)[4];/*q is a pointer to an array of 4 intergers*/
+	p = (int *)a;/*assigns the address of the 1st element of the 2D array 'a' to the pointe 'p'*/
+	q = a;/*Aligns the address of the 1st row of the 2D	array 'a' to the pointer 'q'.
+	So we are giving 'q' an array to point to since it is a pointer to an array*/
 
 	printf("%p %p\n", p, q);
 	p++;
