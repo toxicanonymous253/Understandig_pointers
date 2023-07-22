9baad74fc61fbc50367d5cefddd97060681c679b
toxicfrank <ngugifrancis930@gmai.com>
2023-07-22 10:38:54 +0300

    pointers

--- understanding_pointers/pointer_arrays/8-ptr_arr.c
+++ understanding_pointers/pointer_arrays/8-ptr_arr.c
@@ -5,12 +5,15 @@ int main(void)
 	int a[][4] = {
 		{5, 7, 5, 9},
 		{4, 6, 3, 1},
-		{2, }
+		{2, 9, 0, 6},
 	};
 	int *p;
 	int (*q)[4];
 	p = (int *)a;
 	q = a;
 
-	printf("%p")
+	printf("%p %p\n", p, q);
+	p++;
+	q++;
+	printf("%p %p\n", p, q);
 }
