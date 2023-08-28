2e7d5fcdfa809bfc4a63663326265d0c920b551a
toxicfrank <ngugifrancis930@gmai.com>
2023-08-28 05:06:03 +0300

    pointers

---
+++ understanding_pointers/pointers_structures/acount.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+    struct account{
+        int no;
+        char acc_name[15];
+        float bal;
+    };
+    struct account a1, a2, a3;
+
+    printf("Enter account, nos, name and balances");
+
+    scanf("%d %s %f\n", &a1.no, a1.acc_name, &a1.bal);
+    scanf("%d %s %f\n", &a2.no, a2.acc_name, &a2.bal);
+    scanf("%d %s %f\n", &a3.no, a3.acc_name, &a3.bal);
+
+    printf("%d %s %f", a1.no, a1.acc_name, a1.bal);
+    printf("%d %s %f", a2.no, a2.acc_name, a2.bal);
+    printf("%d %s %f", a3.no, a3.acc_name, a3.bal);
+
+    return (0);
+}
