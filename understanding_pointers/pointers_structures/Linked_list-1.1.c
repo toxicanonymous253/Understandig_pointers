0e2c28746cb2ecddeadd1d03c21b7f23738713f5
toxicfrank <ngugifrancis930@gmai.com>
2023-09-06 12:53:27 +0300

    pointers

--- understanding_pointers/pointers_structures/Linked_list-1.1.c
+++ understanding_pointers/pointers_structures/Linked_list-1.1.c
@@ -6,13 +6,16 @@ typedef struct node
     int data;
     struct node *link;
 }node;
-/*Function protoype*/
 
+/*Function protoype*/
 void append(node **q, int num);
+void addatbeg(node **q, int num);
 
 int main(void)
 {
     node *p = NULL;
+    addatbeg(&p, 6);
+    addatbeg(&p, 9);
     append(&p, 4);
     append(&p, 7);
 
@@ -55,7 +58,59 @@ void append(node **q, int num)
         r->link = NULL;
         temp->link = r;/*the last node tha wast the before will point to the new node created to make a chain of nodes*/
     }
-    printf("%d", num);
-    printf("\n");
     return;
 }
+/**
+ * addatbeg - this function adds a new node at the beginnig of a linked list
+ * @q:pointer that stores the address of a pointer
+ * @num: the number to be added at the beginnig
+ * Return: void 
+ */
+void addatbeg(node **q, int num)
+{
+    node *temp; // Create a new node
+
+    temp = malloc(sizeof(node)); // Allocate memory for the new node
+    if (temp == NULL) {
+        fprintf(stderr, "Memory allocation failed.\n");
+        exit(1);
+    }
+
+    temp->data = num; // Set the data of the new node
+    temp->link = *q; // Make the new node point to the current head
+    *q = temp; // Update the head to point to the new node
+}
+
+/**
+ * addafter - adds a new node after a specified number of nodes
+ * @q: pointer that stores the address of a variable
+ * @loc: location at which a node should be added
+ * @num: the number to be added
+ */
+void addafter(node *q, int loc, int num)
+{
+    int i;
+    node *temp, *r;/*temp will travers the list and r will be used to create a link at a certain position*/
+
+    temp = q;
+    /*traversing the list till we reach the desired destination*/
+    for(i = 0; i < loc; i++)
+    {
+        temp = temp->link;
+
+        /*Checking if list is empy or if it is less than the specified location*/
+        if(temp == NULL)
+        {
+            printf("There are less than %d\n", loc);
+            return;
+        }
+        r = malloc(sizeof(node));
+        r->data = num;
+        r->link = NULL;
+        r->link = temp->link;/*set member of r to point to the node temp was originally pointing at*/
+        temp->link = r;
+    }
+    return;
+}
+
+
